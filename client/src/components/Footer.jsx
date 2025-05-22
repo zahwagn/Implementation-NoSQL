35a40bbd7fee9e183fb58b66d404f52f3ac8b7db
zahwagn <dwiginasittizahwa1216@gmail.com>
2025-05-22 14:15:42 +0700

    frontend

--- client/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    return (
-      <footer className="bg-blue-900 text-white text-center p-4">
-        &copy; {new Date().getFullYear()} Media Tracker Project
-      </footer>
-    );
-  }
-  
